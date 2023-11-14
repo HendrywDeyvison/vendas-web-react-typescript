bdc6baa643088238d39499f8c249abcf19be55a7
HendrywDeyvison <hendryw.deyvison@gmail.com>
2023-11-14 19:16:51 -0300

    feat: create login module

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { BackgroundImage } from "../styles/LoginScreen.style";
+
+const LoginScreen = () =>{
+    return (
+        <div>
+            <BackgroundImage src="./background.png" />
+        </div>
+    );
+
+}
+
+export default LoginScreen;
